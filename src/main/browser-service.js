0199eaf24cd5f9ff39838400713aca5693aaa7d9
B5-Software <b5-software@outlook.com>
2026-08-13 15:26:38 +0800

    fix: 修复头像框全部不加载（拆分模块漏 require fs/path）

--- src/main/browser-service.js
+++ src/main/browser-service.js
@@ -11,6 +11,8 @@
 
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const { BrowserWindow, dialog, screen } = require('electron');
 
 module.exports = function registerPlaywrightIpc({ ipcMain, getSettings, getMainWindow, getImagesDir }) {
